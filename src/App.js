208720a7afc13322903ab09a65d21a15d43c70f7
Arjun Singh <arjunsengar29@gmail.com>
2022-05-05 15:20:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header"
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Home from "./Components/Home";
+import { DataContext } from "./Components/Blog_Context";
+import Display from "./Components/Display";
+import Article from "./Components/Article";
+import "./Components/style.css"
+import Footer from "./Components/Footer";
+import Logo from "./Components/Logo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return(
+        <>
+        <DataContext>
+        <div>
+            <Router>
+                <Logo />
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home"/>}/>
+                    <Route path="/home" element={<Home />}/>
+                    <Route path="/category/:selected" element={<Display />}>
+                        
+                    </Route>
+                    <Route path="/category/:selected/:selectedID" element={<Article />}/>
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+        </DataContext>
+        </>
+    );
+};
 
 export default App;
